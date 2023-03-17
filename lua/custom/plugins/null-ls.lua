5e6b2531bb80c80c193d617aea1a1b57807e5974
Charlie B <charlie.batten@microlise.com>
2023-03-17 14:56:07 +0000

    Added null-ls for eslint_d completion

---
+++ lua/custom/plugins/null-ls.lua
@@ -0,0 +1,13 @@
+return {
+	"jose-elias-alvarez/null-ls.nvim",
+	config = function()
+		local null_ls = require("null-ls")
+		null_ls.setup({
+			debug = false,
+			sources = {
+				null_ls.builtins.diagnostics.eslint_d,
+				null_ls.builtins.formatting.eslint_d
+			}
+		})
+	end
+}
