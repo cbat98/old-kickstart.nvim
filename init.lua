93364d4b282c48f7fcb206ea915c655675cfd137
Charlie Batten <charlie.batten@microlise.com>
2023-07-27 11:12:16 +0100

    Updated init.nvim to add lsps

--- init.lua
+++ init.lua
@@ -362,12 +362,6 @@ end
 --  Add any additional override configuration in the following tables. They will be passed to
 --  the `settings` field of the server config. You must look up that documentation yourself.
 local servers = {
-  -- clangd = {},
-  -- gopls = {},
-  -- pyright = {},
-  -- rust_analyzer = {},
-  -- tsserver = {},
-
   lua_ls = {
     Lua = {
       workspace = { checkThirdParty = false },
@@ -376,6 +370,8 @@ local servers = {
   },
   powershell_es = {},
   angularls = {},
+  csharp_ls = {},
+  tsserver = {}
 }
 
 -- Setup neovim lua configuration
