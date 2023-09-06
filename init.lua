e6dcecaedd51e69cb9e8913926ed31e29ee2d9a3
Charlie B <charlie.batten@microlise.com>
2023-09-06 14:14:48 +0100

    Updated sets

--- init.lua
+++ init.lua
@@ -141,45 +141,6 @@ require('lazy').setup({
   { import = 'custom.plugins' },
 }, {})
 
--- [[ Setting options ]]
-
--- Set highlight on search
-vim.o.hlsearch = false
-
--- Make line numbers default
-vim.wo.number = true
-
--- Enable mouse mode
-vim.o.mouse = 'a'
-
--- Sync clipboard between OS and Neovim.
---  Remove this option if you want your OS clipboard to remain independent.
---  See `:help 'clipboard'`
-vim.o.clipboard = 'unnamedplus'
-
--- Enable break indent
-vim.o.breakindent = true
-
--- Save undo history
-vim.o.undofile = true
-
--- Case-insensitive searching UNLESS \C or capital in search
-vim.o.ignorecase = true
-vim.o.smartcase = true
-
--- Keep signcolumn on by default
-vim.wo.signcolumn = 'yes'
-
--- Decrease update time
-vim.o.updatetime = 250
-vim.o.timeoutlen = 300
-
--- Set completeopt to have a better completion experience
-vim.o.completeopt = 'menuone,noselect'
-
--- NOTE: You should make sure your terminal supports this
-vim.o.termguicolors = true
-
 -- [[ Basic Keymaps ]]
 
 -- Keymaps for better default experience
@@ -443,9 +404,6 @@ cmp.setup {
   },
 }
 
--- The line beneath this is called `modeline`. See `:help modeline`
--- vim: ts=2 sts=2 sw=2 et
-
 -- Enable relative line numbers
 vim.opt.number = true
 vim.opt.relativenumber = true
@@ -453,6 +411,10 @@ vim.opt.relativenumber = true
 -- Enable nice search highlight
 vim.opt.hlsearch = false
 vim.opt.incsearch = true
+
+-- Case-insensitive searching UNLESS \C or capital in search
+vim.o.ignorecase = true
+vim.o.smartcase = true
 
 -- Disable clipboard syncing
 vim.opt.clipboard = ""
@@ -474,3 +436,19 @@ vim.opt.scrolloff = 8
 vim.opt.swapfile = false
 
 vim.opt.updatetime = 50
+
+-- Enable mouse mode
+vim.o.mouse = 'a'
+
+-- Save undo history
+vim.o.undofile = true
+
+-- Keep signcolumn on by default
+vim.wo.signcolumn = 'yes'
+
+-- Set completeopt to have a better completion experience
+vim.o.completeopt = 'menuone,noselect'
+
+-- Pretty colours
+vim.o.termguicolors = true
+
