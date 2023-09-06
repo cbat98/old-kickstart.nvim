7c63eb6fbb1ca6fc87e72031dac9b9cf9fb58d41
Charlie B <charlie.batten@microlise.com>
2023-09-06 15:22:46 +0100

    Added remaps to delete to void buffer

--- lua/custom/remap.lua
+++ lua/custom/remap.lua
@@ -4,3 +4,6 @@ vim.keymap.set("v", "<leader>y", "\"+y", { desc = "[y]ank to system clipboard" }
 vim.keymap.set("n", "<leader>p", "\"+p", { desc = "[p]aste from system clipboard" })
 vim.keymap.set("n", "<leader>P", "\"+P", { desc = "[P]aste from system clipboard" })
 
+vim.keymap.set("n", "<leader>d", "\"_d", { desc = "[d]elete to void buffer" })
+vim.keymap.set("v", "<leader>d", "\"_d", { desc = "[d]elete to void buffer" })
+
