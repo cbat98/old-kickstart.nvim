0889451288ebe81a77977e1188b135c4bda48742
Charlie B <charlie.batten@microlise.com>
2023-09-06 15:22:36 +0100

    Added remaps for system clipboard

---
+++ lua/custom/remap.lua
@@ -0,0 +1,6 @@
+vim.keymap.set("n", "<leader>y", "\"+y", { desc = "[y]ank to system clipboard" })
+vim.keymap.set("v", "<leader>y", "\"+y", { desc = "[y]ank to system clipboard" })
+
+vim.keymap.set("n", "<leader>p", "\"+p", { desc = "[p]aste from system clipboard" })
+vim.keymap.set("n", "<leader>P", "\"+P", { desc = "[P]aste from system clipboard" })
+
