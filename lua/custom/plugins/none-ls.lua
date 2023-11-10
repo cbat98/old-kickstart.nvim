e4d36857d11387779f0ec6c3a5b7b60ca0a81663
Charlie Batten <charlie.batten@microlise.com>
2023-11-10 12:06:45 +0000

    Migrated null-ls to none-ls

---
+++ lua/custom/plugins/none-ls.lua
@@ -0,0 +1,15 @@
+return {
+	"nvimtools/none-ls.nvim",
+	init = function()
+		local null_ls = require("null-ls")
+		null_ls.setup({
+			sources = {
+				null_ls.builtins.diagnostics.eslint_d,
+                null_ls.builtins.formatting.eslint_d,
+                null_ls.builtins.diagnostics.markdownlint_cli2.with {
+                    args = { "$FILENAME" }
+                }
+			},
+		})
+	end
+}
