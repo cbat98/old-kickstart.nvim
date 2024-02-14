b3029c984fb8d58a45c07a6ffee274227aefaf55
Charlie Batten <charlie.batten@microlise.com>
2024-02-14 10:07:35 +0000

    Config markdown-toc

--- lua/custom/plugins/markdown-toc.lua
+++ lua/custom/plugins/markdown-toc.lua
@@ -1,3 +1,9 @@
 return {
-    "https://github.com/mzlogin/vim-markdown-toc.git"
+    "https://github.com/mzlogin/vim-markdown-toc.git",
+    init = function()
+        vim.g.vmt_fence_text = "markdown-toc-start"
+        vim.g.vmt_fence_closing_text = "markdown-toc-end"
+        vim.g.vmt_list_item_char = "-"
+        vim.g.vmt_list_indent_text = '  '
+    end
 }
